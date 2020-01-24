a57ef1ced9937c058c7405b6752cd8547a77b7bc
Nik Kalyani <nik@kalyani.com>
2020-01-24 14:24:25 -0800

    Dupe ETH config to HMY

--- ~blockchains/harmony/truffle-config.js
+++ ~blockchains/harmony/truffle-config.js
@@ -1,1 +1,28 @@
 //urge clog right example dish drill card maximum mix bachelor section select
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+
+let devUrl = 'http://127.0.0.1:7545/';
+let mnemonic = 'PLACEHOLDER';   ///@{ "___test-mnemonic___": "PLACEHOLDER"}
+let testAccounts = null;  ///@{ "___test-accounts___": "null"}
+
+module.exports = {
+  networks: {
+    development: {
+      url: devUrl,  // Required for DappStarter config generation
+      provider: () => new HDWalletProvider(
+                                      testAccounts,
+                                      devUrl,           // provider url
+                                      0,                // address index
+                                      10,               // number of addresses
+                                      true,             // share nonce
+                                      `m/44'/60'/0'/0/` // wallet HD path
+                                    ),
+      network_id: '*'
+    }
+  },
+  compilers: {
+    solc: {
+      version: '^0.5.11'
+    }
+  }
+};
