02b8eecbb569d9c593f31c11b71f4488c173c284
Jonathan Sheely <jsheely@thirtytech.net>
2020-04-09 22:08:57 -0400

    Switched to correct contract

--- ~languages/solidity/packages/dapplib/test/dapp-tests.js
+++ ~languages/solidity/packages/dapplib/test/dapp-tests.js
@@ -8,15 +8,22 @@ contract('Dapp Cross-contract Tests', async (accounts) => {
     let testDappStateContract;
     let testDappContract;
     let testAccounts;
+    let config = null
   
     before('setup contract', async () => {
         testDappStateContract = await DappStateContract.new();
         testDappContract = await DappContract.new(dappStateContract.address);
         testAccounts = accounts;
+        DappLib.getConfig = Function(`return ${ JSON.stringify(DappLib.getTestConfig(testDappStateContract, testDappContract, testAccounts))}`);
+
+        // Call the re-written function to get the test config values
+        config = DappLib.getConfig();
+        config.testDappStateContract = testDappStateContract;
+        config.testDappContract = testDappContract;
     });
 
     it(`has correct result from sample cross-contract call`, async function () {
-      let stateContractOwner = await config.dappContract.getStateContractOwner.call();
+      let stateContractOwner = await config.testDappContract.getStateContractOwner.call();
       assert.equal(stateContractOwner, config.owner, "Incorrect cross-contract result");
   });
 
