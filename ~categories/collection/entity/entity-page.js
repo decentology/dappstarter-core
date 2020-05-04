e8ad04b6e1b633c19877dc2f9bf4eab591ad9260
Traci Fong <tre.fong1@gmail.com>
2020-05-04 14:04:28 -0700

    Entity block working

--- ~categories/collection/entity/entity-page.js
+++ ~categories/collection/entity/entity-page.js
@@ -2,9 +2,11 @@
 
 import '../../lib/components/shared/action-card.js';
 import './components/page-body.js';
-import '../../lib/components/widgets/text-widget.js';
 import "./components/page-panel.js";
+import '../../lib/components/widgets/text-widget.js';
 import '../../lib/components/widgets/number-widget.js';
+import '../../lib/components/widgets/account-widget.js';
+import DOM from "../../lib/components/shared/dom";
 import DappLib from "@trycrypto/dappstarter-dapplib";
 import { LitElement, html, customElement, property } from "lit-element";
 
@@ -20,7 +22,7 @@ export default class EntityPage extends LitElement {
     createRenderRoot() {
         return this;
     }
-  
+
     constructor(args) {
         super(args);
         this.eventHandlerRegistered = false;
@@ -29,155 +31,132 @@ export default class EntityPage extends LitElement {
             resultsPerPage: 10
         }
 
-        setTimeout(() => {
+        setTimeout(async () => {
+            const response = await DappLib.getEntityCount();
+            const json = await response;
+            this.entitiesCount = json.result;
+    
+            DOM.elid('pageNumbers').innerHTML = this.showPageNumbers() + this.showResultsPerPage();
 
-            // DOM.elid('page').addEventListener('change', (e) => {
-            //     console.log("page change value", e.target.value)
-            //     DOM.elid('page').setAttribute('data-field', 'page');
-            // });
-    
-            // DOM.elid('results').addEventListener('change', (e) => {
-            //     console.log("results per page change")
-            //     DOM.elid('results').setAttribute('data-field', 'resultsPerPage');
-            // });
-    
-            // document.querySelector("action-button[action=setEntity]").addEventListener('click', (e) => {
-            //     console.log("set entity click")
-            //     this.connectedCallback();
-            // });
-    
-        }, 0)
+        }, 0);
     }
 
-    async fetchCount(){
-        const response = await DappLib.getEntityCount();
-        const json = await response;
-        this.entitiesCount = json.result;
-    }
-
-//     async connectedCallback() {
-//         await this.fetchCount();
-//         this.render();
-
-// i    }
-
-    showPageNumbers(){
+    showPageNumbers() {
         let optionsString = '';
-        if(parseInt(this.entitiesCount) % this.pagesInfo.resultsPerPage >= 1 ){
+        if (parseInt(this.entitiesCount) % this.pagesInfo.resultsPerPage >= 1) {
             //more than one page of results
-            let pageNumbers = Math.ceil(parseInt(this.entitiesCount)/this.pagesInfo.resultsPerPage);
+            let pageNumbers = Math.ceil(parseInt(this.entitiesCount) / this.pagesInfo.resultsPerPage);
 
             for (let step = 1; step <= pageNumbers; step++) {
-                optionsString = optionsString + '<option value="' + `${step}` + '">' + `${step}`+ '</option>'
+                optionsString = optionsString + '<option value="' + `${step}` + '">' + `${step}` + '</option>'
             }
-        }else {
+        } else {
             //less than one page of results
             optionsString = '<option value="1">1</option>'
         }
-            return '<div><label for="page">Page Number:</label><select id="page" data-field="page">' + optionsString + '</select></div>'
+        return '<div style="display: inline-block; margin-right: 20pt;"><label for="page">Page Number:</label><select id="page" data-field="page">' + optionsString + '</select></div>'
     }
 
-    showResultsPerPage(){
+    showResultsPerPage() {
         return (
-            '<div><label for="results">Results Per Page:</label><select id="results" data-field="resultsPerPage"><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="40">40</option> </select></div>'
+            '<div style="display: inline-block"><label for="results">Results Per Page:</label><select id="results" data-field="resultsPerPage"><option value="10">10</option><option value="20">20</option><option value="30">30</option><option value="40">40</option> </select></div>'
         )
     }
 
     render() {
         return html`
          <page-body
-  title="${this.title}"
-  category="${this.category}"
-  description="${this.description}"
->
-  <action-card
-    title="Get Entity"
-    description="Get entity details"
-    action="getEntity"
-    method="get"
-    fields="id"
-  >
-    <number-widget field="id" label="Entity ID" placeholder="Entity ID">
-    </number-widget>
-  </action-card>
+            title="${this.title}"
+            category="${this.category}"
+            description="${this.description}"
+        >
+            <action-card
+                title="Get Entity"
+                description="Get entity details"
+                action="getEntity"
+                method="get"
+                fields="id"
+            >
+                <number-widget field="id" label="Entity ID" placeholder="Entity ID">
+                    </number-widget>
+                </action-card>
 
-  <action-card
-    title="Get Entities by Creator"
-    description="Get all Entity Ids where Account is the creator"
-    action="getEntitiesByCreator"
-    method="get"
-    fields="account"
-  >
-    <account-widget
-      field="account"
-      label="Account"
-      placeholder="Account address"
-    >
-    </account-widget>
-  </action-card>
+                <action-card
+                    title="Get Entities by Creator"
+                    description="Get all Entity Ids where Account is the creator"
+                    action="getEntitiesByCreator"
+                    method="get"
+                    fields="account"
+                >
+                <account-widget
+                    field="account"
+                    label="Account"
+                    placeholder="Account address"
+                >
+                </account-widget>
+            </action-card>
 
-  <action-card
-    title="Get Entities by Page"
-    description="Get all Entities by page"
-    action="getEntitiesByPage"
-    method="get"
+            <action-card
+                title="Get Entities by Page"
+                description="Get all Entities by page"
+                action="getEntitiesByPage"
+                method="get"
 
-    fields="page resultsPerPage"
-  >
-    ${this.showResultsPerPage()} ${this.showPageNumbers()}
-  </action-card>
+                fields="page resultsPerPage"
+            >
+                <div id="pageNumbers"></div>
+            </action-card>
 
-  <action-card
-    title="Add Entity"
-    description="Set entity details"
-    action="setEntity"
-    method="post"
-    fields="title count"
-  >
-    <text-widget
-      field="title"
-      label="Title"
-      placeholder="Title for entity"
-    >
-    </text-widget>
+            <action-card
+                title="Add Entity"
+                description="Set entity details"
+                action="setEntity"
+                method="post"
+                fields="title count"
+            >
+                <text-widget
+                    field="title"
+                    label="Title"
+                    placeholder="Title for entity"
+                >
+                </text-widget>
 
-    <number-widget
-      field="count"
-      label="Count"
-      placeholder="Count for entity"
-    >
-    </number-widget>
-  </action-card>
+                <number-widget
+                    field="count"
+                    label="Count"
+                    placeholder="Count for entity"
+                >
+                </number-widget>
+            </action-card>
 
-  <action-card
-    title="Update Entity"
-    description="Set entity details"
-    action="setEntity"
-    method="post"
-    fields="title count id"
-  >
-    <number-widget field="id" label="Entity ID" placeholder="Entity ID">
-    </number-widget>
+            <action-card
+                title="Update Entity"
+                description="Set entity details"
+                action="setEntity"
+                method="post"
+                fields="title count id"
+            >
+                <number-widget field="id" label="Entity ID" placeholder="Entity ID">
+                </number-widget>
 
-    <text-widget
-      field="title"
-      label="Title"
-      placeholder="Title for entity"
-    >
-    </text-widget>
+                <text-widget
+                    field="title"
+                    label="Title"
+                    placeholder="Title for entity"
+                >
+                </text-widget>
 
-    <number-widget
-      field="count"
-      label="Count"
-      placeholder="Count for entity"
-    >
-    </number-widget>
-  </action-card>
-</page-body>
-<page-panel id="resultPanel"></page-panel>
-
+                <number-widget
+                    field="count"
+                    label="Count"
+                    placeholder="Count for entity"
+                >
+                </number-widget>
+            </action-card>
+        </page-body>
+    <page-panel id="resultPanel"></page-panel>
      `;
     }
-} 
+}
 ///)
 
