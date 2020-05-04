5688411d72f6ba5a037527799b08af67ffbb959f
Traci Fong <tre.fong1@gmail.com>
2020-05-04 14:04:06 -0700

    Create getItemsByPage global function

--- ~languages/solidity/packages/dapplib/contracts/DappLib.sol
+++ ~languages/solidity/packages/dapplib/contracts/DappLib.sol
@@ -6,6 +6,52 @@ library DappLib {
 
 ///+library
     
+function getItemsByPage
+                            (
+                                uint256 page, 
+                                uint256 resultsPerPage,
+                                bytes32[] memory itemList
+
+                            ) 
+                            internal 
+                            pure
+                            returns(bytes32[] memory) 
+{
+    /*
+        Source: https://medium.codylamson.com/how-to-paginate-smart-contract-array-returns-cd6227479aa3
+        ex: _page 1, _resultsPerPage 20 | 1 * 20 - 20 = 0
+        ex2: _page 2, _resultsPerPage 20 | 2 * 20 - 20 = 20
+        starting point for listing items in array
+    */
+
+    uint256 index = sub(mul(resultsPerPage,page),resultsPerPage);
+
+    // Return emptry array if already empty or index is out of bounds
+    if ((itemList.length == 0) || (index > sub(itemList.length, 1))) {
+        return new bytes32[](0);
+    }
+
+    // Create fixed length array because we cannot push to array in memory
+    bytes32[] memory itemPage = new bytes32[](resultsPerPage);
+
+    
+    uint256 returnCounter = 0; 
+    
+    for(index; index < (mul(resultsPerPage, page)); index++) {
+        if (index < (itemList.length)) {
+            itemPage[returnCounter] = itemList[index];
+        } else {
+            itemPage[returnCounter] = bytes32(0);
+        }
+
+        returnCounter++;
+    }
+
+    return itemPage;
+}
+
+
+
 // *** BEGIN SafeMath -- Copyright (c) 2016 Smart Contract Solutions, Inc. ***
 
 // It's important to avoid vulnerabilities due to numeric overflow bugs
