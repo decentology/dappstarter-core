a05680d92660afc925eb47804ddf539333c62534
Nik Kalyani <nik@kalyani.com>
2020-07-21 05:22:46 -0700

    Flow voting start

---
+++ ~categories/voting/ballot/ballot-page.js
@@ -0,0 +1,90 @@
+///(page-post-content
+import "./components/page-panel.js";
+import "./components/page-body.js";
+import "../../lib/components/shared/action-card.js";
+import "../../lib/components/widgets/account-widget.js";
+import "../../lib/components/widgets/text-widget.js";
+import "../../lib/components/widgets/number-widget.js";
+import DappLib from "@trycrypto/dappstarter-dapplib";
+import { LitElement, html, customElement, property } from "lit-element";
+
+@customElement('ballot-page')
+export default class BallotPage extends LitElement {
+  @property()
+  title;
+  @property()
+  category;
+  @property()
+  description;
+
+  createRenderRoot() {
+    return this;
+  }
+
+  constructor(args) {
+    super(args);
+  }
+
+  render() {
+    let content = html`
+      <page-body
+        title="${this.title}"
+        category="${this.category}"
+        description="${this.description}"
+      >
+
+        <action-card
+          title="Account Query Example"
+          description="An example of how to query an account during development"
+          action="getAccountInfo"
+          method="get"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+
+        <action-card
+          title="Initialize Account"
+          description="Initializes an account so it can receive a NFT"
+          action="initializeAccount"
+          method="post"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+        <action-card
+          title="Get NFT IDs"
+          description="Get NFT IDs for Account"
+          action="getIDs"
+          method="get"
+          fields="account"
+        >
+          <account-widget
+            field="account"
+            label="Account"
+            placeholder="Account address"
+          >
+          </account-widget>
+        </action-card>
+
+
+      </page-body>
+      <page-panel id="resultPanel"></page-panel>
+    `;
+
+    return content;
+  }
+}
+///)
