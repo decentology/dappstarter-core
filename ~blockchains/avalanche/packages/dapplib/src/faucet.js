7f9434b3ba89537fffe5bb2e899d5d8c931d36e3
Nik Kalyani <nik@kalyani.com>
2021-05-15 07:07:25 -0700

    Avalanche faucet support

--- ~blockchains/avalanche/packages/dapplib/src/faucet.js
+++ ~blockchains/avalanche/packages/dapplib/src/faucet.js
@@ -4,33 +4,37 @@ const Web3 = require("web3");
 const BN = require('bn.js');
 
 let devUri = TruffleConfig.networks.development.uri;
-let faucetUri = 'https://api.faucet.matic.network/getTokens';
+let faucetUri = 'https://faucet.avax-test.network/api/token_custom';
 let testAccounts = TruffleConfig.networks.development.provider().addresses;
 
+if (process.argv.length > 2) {
+    testAccounts = testAccounts.slice(0, parseInt(process.argv[2]));
+}
 
 let web3 = new Web3(new Web3.providers.HttpProvider(devUri));
 
-// We want to keep accounts loaded with test Matic so we fire and forget requests to the faucet
+// We want to keep accounts loaded with test tokens so we fire and forget requests to the faucet
 testAccounts.map((account, index) => {
-
+    
     web3.eth
         .getBalance(account)
         .then((result) => {
-            
+            console.log(index, account, result)
             let balance = new BN(String(result));
-            let target = new BN('1000000000000000000');
+            let target = new BN('1000000000');
             if (balance.lt(target)) {
                 console.log(`Faucet request for (${index}) ${account} (Current Balance: ${balance.toString()})`);
                 setTimeout(() => {
                     request.post(
                         faucetUri,
                         {
-                        json: {
-                            network: "mumbai",
-                            address: account,
-                            token: "maticToken"
-                        },
+                            json: {
+                                key: "3XT9XY8-AM4MBW8-NRB8RSX-H3HG2QJ",
+                                to: account,
+                                amount: 1000000000
+                            },
                         }, (error, res, body) => {
+                            console.log(error, res, body)
                         if (error) {
                             console.error(error);
                         }
