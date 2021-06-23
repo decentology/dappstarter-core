d7c74974474416070e582ed59cf47c721ff48918
jacob-tucker <jacobtucker818@gmail.com>
2021-06-23 16:06:29 -0400

    Fix top-navigation local storage bug

--- packages/client/src/components/top-navigation.js
+++ packages/client/src/components/top-navigation.js
@@ -11,7 +11,7 @@ export default class TopNavigation extends LitElement {
   constructor(args) {
     super(args);
     let page = localStorage.getItem('dappstarter-page');
-    if (page.includes('-')) {
+    if (page && page.includes('-')) {
       page = 'harness';
     }
     setTimeout(() => {
